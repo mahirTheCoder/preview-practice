6b7028cf954319d2dde6a7e2da5d0ad9677f7c09
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-07-03 20:41:02 +0600

    slider complete

--- js/script.js
+++ js/script.js
@@ -2,3 +2,51 @@ $('.counter').counterUp({
     delay: 10,
     time: 1000
 });
+// -----------------------
+
+
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  autoplay: true,
+  autoplaySpeed: 3000,
+//   dots: true,
+});
+	
+
+
+// ------------------------------
+ $('.Sliderfor').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: '.slidernav',
+  arrows: false,
+});
+$('.slidernav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.Sliderfor',
+  focusOnSelect: true,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
